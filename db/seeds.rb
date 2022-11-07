af512d7f3f9d1998a077f513a6e5d9bb9d710d93
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-07 16:39:04 +0100

    add database schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: "oyin")
+recipe = Recipe.create(name:"Maggi", preparation_time: "5 minutes", cooking_time:"20 minutes", description:"this a maggi", public: true, user_id:user.id)
+food = Food.create(name:"tomatos", measurement: "grams",price: 12,quantity: 3, user_id:user.id)
+recipe_food = RecipeFood.create(quantity:4, food_id:food.id, recipe_id:recipe.id )
