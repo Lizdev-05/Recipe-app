707c4e3793056b646187aa2505d6952848077c31
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-07 19:32:21 +0100

    Bundle devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  #  Default URL options for the Devise mailer in each environment.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
