8790578d14d93e0254073101d3f87063bc5614b7
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-08 09:23:23 +0100

    Validate food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
     belongs_to :user
     has_many :recipe_foods, dependent: :destroy
+
+    validates :name, presence: true, length: { maximum: 50 }
+    validate :measurement, presence: true, length: { maximum: 20 }
+    validate :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+    validate :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
