ad41e83a005490ed69bf9cb88e9e75add67f4fcb
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-08 17:33:16 +0200

    update recipe style

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,6 +13,7 @@ class RecipesController < ApplicationController
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+        format.js
       else
         format.html { render :new, alert: 'An error has occurred while creating the recipt, try again' }
       end
@@ -21,11 +22,13 @@ class RecipesController < ApplicationController
 
   def destroy
     @recipe = Recipe.find(params[:id])
-    if @recipe.delete
-      format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was deleted successfully.' }
+    respond_to do |format|
+    if @recipe.destroy
+      format.html { redirect_to recipes_path, notice: 'Recipe was deleted successfully.' }
     else
       format.html { render :new, alert: 'An error has occurred while deleting the recipt, try again' }
     end
+  end
   end
 
   def recipe_params
