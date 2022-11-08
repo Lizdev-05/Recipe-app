d78057c6120679762053c0032e444183290ed961
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-08 21:12:17 +0200

    add show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
