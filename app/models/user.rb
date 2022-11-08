a9c9e4a1e33071137617b4bb3aed61364fc2469b
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-08 09:10:52 +0100

    Add relationship to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy 
+  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
