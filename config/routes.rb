08d4bbc5f0ea00b3d52b1d6c8db7ac6b34d200f1
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-08 21:13:56 +0200

    add routes for recipe foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, sign_out_via: [:get, :post]
   resources :users, only: [:index]
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+  end
   resources :foods, only: [:index]
   root to: "foods#index"
 end
