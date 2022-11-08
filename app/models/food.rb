dde2475a370fbdaa291a94f2fa8723309d7c0c6a
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-08 09:26:13 +0100

    Fix linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,9 @@
 class Food < ApplicationRecord
-    belongs_to :user
-    has_many :recipe_foods, dependent: :destroy
+  belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
 
-    validates :name, presence: true, length: { maximum: 50 }
-    validate :measurement, presence: true, length: { maximum: 20 }
-    validate :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validate :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validate :measurement, presence: true, length: { maximum: 20 }
+  validate :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
